485606b48f695b0ce8eb9898b2fcd6c379f555b1
Chartchai <chartchai.d@cmu.ac.th>
2021-09-08 11:12:16 +0700

    change type to base input

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -2,40 +2,28 @@
   <div>
     <h1>Create an event</h1>
     <form @submit.prevent="saveEvent">
-      <label>Category</label>
-      <input
-        v-model="event.category"
-        type="text"
-        placeholder="Category"
-        class="field"
-      />
+    <BaseInput 
+        v-model="event.category" 
+        type="text" 
+        label="Category" />
       <h3>Name & describe your event</h3>
 
-      <label>Title</label>
-      <input
-        v-model="event.title"
-        type="text"
-        placeholder="Title"
-        class="field"
-      />
+      <BaseInput 
+        v-model="event.title" 
+        type="text" 
+        label="Title" />
 
-      <label>Description</label>
-      <input
-        v-model="event.description"
-        type="text"
-        placeholder="Description"
-        class="field"
-      />
+      <BaseInput 
+        v-model="event.description" 
+        type="text" 
+        label="Description" />
 
       <h3>Where is your event?</h3>
 
-      <label>Location</label>
-      <input
-        v-model="event.location"
-        type="text"
-        placeholder="Location"
-        class="field"
-      />
+      <BaseInput 
+        v-model="event.location" 
+        type="text" 
+        label="Location" />
       <button type="submit">Submit</button>
     </form>
 
@@ -44,8 +32,12 @@
 </template>
 <script>
 import EventService from '@/services/EventService.js'
+import BaseInput from '@/components/BaseInput'
 export default {
   inject: ['GStore'],
+  components: {
+    BaseInput
+  },
   data() {
     return {
       event: {
