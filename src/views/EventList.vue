f4b15861024d8178e8ef6fae57a81a37daa066a1
Chartchai <chartchai.d@cmu.ac.th>
2021-09-15 13:27:38 +0700

    add search text

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,11 @@
 <template>
   <h1>Events For Good</h1>
+
   <div class="events">
+    <div class="search-box">
+      <BaseInput v-model="keyword" type="text" label="Search..." />
+    </div>
+
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <div class="pagination">
       <router-link
@@ -44,7 +49,8 @@ export default {
   data() {
     return {
       events: null,
-      totalEvents: 0 // <--- Added this to store totalEvents
+      totalEvents: 0, // <--- Added this to store totalEvents
+      keyword: null
     }
   },
 
@@ -87,6 +93,11 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.search-box {
+  
+  width: 300px;
+  
 }
 .pagination {
   display: flex;
