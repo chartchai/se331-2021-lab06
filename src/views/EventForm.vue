a1a5ef29856142181a18082e1c6a6aca25d7cb7a
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2021-09-20 23:03:40 +0700

    save the images url to backend

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -44,7 +44,8 @@ export default {
         title: '',
         description: '',
         location: '',
-        organizer: { id: '', name: '' }
+        organizer: { id: '', name: '' },
+        imageUrls: []
       },
       files: []
     }
@@ -53,31 +54,29 @@ export default {
     saveEvent() {
       console.log(this.files)
 
-      Promise.all(        
-          this.files.map((file) => {
-            return EventService.uploadFile(file)
-          })        
+      Promise.all(
+        this.files.map((file) => {
+          return EventService.uploadFile(file)
+        })
       ).then((response) => {
-        console.log(response.map((r) => r.data))
-        console.log('finish upload file')
-      })
-
-      EventService.saveEvent(this.event)
-        .then((response) => {
-          console.log(response)
-          this.$router.push({
-            name: 'EventLayout',
-            params: { id: response.data.id }
+        this.event.imageUrls = response.map((r) => r.data)
+        EventService.saveEvent(this.event)
+          .then((response) => {
+            console.log(response)
+            this.$router.push({
+              name: 'EventLayout',
+              params: { id: response.data.id }
+            })
+            this.GStore.flashMessage =
+              'You are successfully add a new event for ' + response.data.title
+            setTimeout(() => {
+              this.GStore.flashMessage = ''
+            }, 3000)
           })
-          this.GStore.flashMessage =
-            'You are successfully add a new event for ' + response.data.title
-          setTimeout(() => {
-            this.GStore.flashMessage = ''
-          }, 3000)
-        })
-        .catch(() => {
-          this.$router.push('NetworkError')
-        })
+          .catch(() => {
+            this.$router.push('NetworkError')
+          })
+      })
     },
     handleImages(files) {
       this.files = files
