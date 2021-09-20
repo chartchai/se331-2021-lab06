df738d339bda8484802e17cb4e5019f151c7abc9
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2021-09-20 22:36:36 +0700

    use map to upload all files

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -45,11 +45,23 @@ export default {
         description: '',
         location: '',
         organizer: { id: '', name: '' }
-      }
+      },
+      files: []
     }
   },
   methods: {
     saveEvent() {
+      console.log(this.files)
+
+      Promise.all(        
+          this.files.map((file) => {
+            return EventService.uploadFile(file)
+          })        
+      ).then((response) => {
+        console.log(response)
+        console.log('finish upload file')
+      })
+
       EventService.saveEvent(this.event)
         .then((response) => {
           console.log(response)
@@ -68,7 +80,7 @@ export default {
         })
     },
     handleImages(files) {
-      console.log(files)
+      this.files = files
     }
   }
 }
