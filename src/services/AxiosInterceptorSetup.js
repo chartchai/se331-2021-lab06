997d2b4ef476bad43ab289d85b7bbfdfc5c277c5
Chartchai <chartchai.d@cmu.ac.th>
2021-09-29 09:19:57 +0700

    add menu bar

--- src/services/AxiosInterceptorSetup.js
+++ src/services/AxiosInterceptorSetup.js
@@ -2,7 +2,7 @@ import apiClient from '@/services/AxiosClient.js'
 apiClient.interceptors.request.use(
   (request) => {
     const token = localStorage.getItem('token')
-    console.log(token)
+
     if (token) {
       request.headers['Authorization'] = 'Bearer ' + token
     }
